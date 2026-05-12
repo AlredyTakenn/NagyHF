d2bc923f35ceea070cd38714484d420ae1ff3aa1
Bence Hunor Rapcsok <rapcsok.bence@edu.bme.hu>
2026-05-12 17:40:33 +0200

    6

---
+++ NagyHF/Hotel.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Container.h"
+#include "Guest.h"
+#include "Reservation.h"
+#include "Room.h"
+#include <string>
+class Hotel
+{
+private:
+	Container<Room*> roomList;
+	Container<Guest> guestList;
+	Container<Reservation> reservationList;
+public:
+	Hotel();
+	Hotel(const Hotel& other) = delete;
+	Hotel& operator=(const Hotel& other) = delete;
+
+	void AddRoom(Room proom);
+	void RegisterGuest(const Guest& pguest);
+	void BookReservation(std::chrono::year_month_day pfrom, std::chrono::year_month_day pto, Guest* pguests, unsigned pguestcount, std::string* pextraservices, unsigned pextraservicescount, unsigned requestedRoomNumber);
+
+	void CheckIn(std::string searchGuestId);
+	void CheckOut(std::string searchGuestId);
+	void GenerateInvoice(std::string searchGuestId);
+
+	~Hotel();
+};
+
